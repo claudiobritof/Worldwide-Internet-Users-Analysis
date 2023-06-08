b2f6df838ebbeb2c57baf4a0a3b44d7709e78d73
Cláudio Brito <cabf.brito@gmail.com>
2023-06-08 09:42:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 # importing pandas and numpy, creating DataFrames "users" and "speed" and setting some preferences:
 import pandas as pd
 import numpy as np
+
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
