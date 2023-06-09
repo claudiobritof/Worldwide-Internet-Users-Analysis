64fb47e1fd4a6d5420d218034cbc6474608be4de
Cláudio Brito <cabf.brito@gmail.com>
2023-06-09 09:51:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import numpy as np
 
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
+#Ajeitar arquivo
 df_users = pd.read_csv(
     r"C:\Users\cabfb\OneDrive\Documentos\Coding\Projetos\Python\Worldwide Internet Users Analysis\datasets\users.csv")
