d67a66c6f7cb655422e03e4ae5bc9b24d02ca9f3
Cláudio Brito <cabf.brito@gmail.com>
2023-06-13 11:36:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# importing pandas and numpy, creating DataFrames "users" and "speed" and setting some preferences:
+#Importing pandas and numpy, creating DataFrames "users" and "speed" and setting some preferences:
 import pandas as pd
 import numpy as np
 
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
-#Ajeitar arquivo
+#Setting files and data:
 df_users = pd.read_csv(
-    r"C:\Users\cabfb\OneDrive\Documentos\Coding\Projetos\Python\Worldwide Internet Users Analysis\datasets\users.csv")
+    r"C:\\Users\\cabfb\\OneDrive\\Documentos\\Coding\\Projetos\\Python\\Worldwide-Internet-Users-Analysis\\datasets\\users.csv")
 df_speed = pd.read_csv(
-    r"C:\Users\cabfb\OneDrive\Documentos\Coding\Projetos\Python\Worldwide Internet Users Analysis\datasets\avgspeed.csv")
+    r"C:\\Users\\cabfb\\OneDrive\\Documentos\\Coding\\Projetos\\Python\\Worldwide-Internet-Users-Analysis\\datasets\\avgspeed.csv")
