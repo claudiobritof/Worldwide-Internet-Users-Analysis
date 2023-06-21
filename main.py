87eae925d71988601fb7e96e6ab83e6781841e3a
Cláudio Brito <cabf.brito@gmail.com>
2023-06-20 23:36:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-#Importing pandas and numpy, creating DataFrames "users" and "speed" and setting some preferences:
+# Importing pandas and numpy, creating DataFrames "users" and "speed" and setting some preferences:
 import pandas as pd
 import numpy as np
 
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
-#Setting files and data:
+# Setting files and data:
 df_users = pd.read_csv(
     r"C:\\Users\\cabfb\\OneDrive\\Documentos\\Coding\\Projetos\\Python\\Worldwide-Internet-Users-Analysis\\datasets\\users.csv")
 df_speed = pd.read_csv(
     r"C:\\Users\\cabfb\\OneDrive\\Documentos\\Coding\\Projetos\\Python\\Worldwide-Internet-Users-Analysis\\datasets\\avgspeed.csv")
 
 df_speed.rename(columns={'Avg \n(Mbit/s)Ookla': 'Avg'}, inplace=True)
+
+# Creating a class to return to me which "continent" each "country" belongs, given a '.csv' file:
